f063b5c80de45bbd077d506b02c0a0e56bc03cdb
Robert Messerle <robertmesserle@gmail.com>
2014-11-19 18:16:36 -0800

    refactor(style): this restyles parts of the site's layout
    enhance(style): added angular logo and switched to red theme
    
    enhance(style): updated toolbar colors for red theme
    
    enhance(ripple): adds a minimal timeout to make sure that updates are complete before creating ripple
    
    feat(theming): adds md-theme-levels directive
    
    enhance(nav): sets focus state for nav items
    
    enhance(button): updates button styles to more closely match material spec

--- src/components/icon/icon.js
+++ src/components/icon/icon.js
@@ -29,14 +29,41 @@ angular.module('material.components.icon', [
  * </hljs>
  *
  */
-function mdIconDirective() {
+function mdIconDirective($http, $window) {
+  var dummy = angular.element('<div>'),
+      lookup = {};
   return {
     restrict: 'E',
-    template: '<object class="md-icon"></object>',
-    compile: function(element, attr) {
-      var object = angular.element(element[0].children[0]);
-      if(angular.isDefined(attr.icon)) {
-        object.attr('data', attr.icon);
+    link: function(scope, element, attr) {
+      var icon = attr.icon,
+          svg;
+      if (!icon) return;
+
+      if (lookup[icon]) $timeout(function () { writeHTML(lookup[icon]); }, 0, false);
+      else $http.get(icon).success(handleSuccess);
+
+      function handleSuccess (xml) {
+        var color = $window.getComputedStyle(element[0]).color;
+        dummy.html(xml);
+        svg = dummy.find('svg');
+        dummy.empty().append(svg);
+        writeHTML(dummy.html());
+      }
+
+      function writeHTML(html) {
+        lookup[icon] = html;
+        element.html(html);
+        colorizeIcon(element);
+        function colorizeIcon(elem) {
+          angular.forEach(elem.children(), function (child) {
+            //-- wrap child with jqLite
+            child = angular.element(child);
+            //-- if no fill property, add one
+            if (!child.attr('fill')) child.attr({ fill: color });
+            //-- parse children
+            colorizeIcon(child);
+          });
+        }
       }
     }
   };
